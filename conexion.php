f7885c5401de0d2c80c8f39ceaee3662ef044a5b
Alexander <alexandercs740@gmail.com>
2021-02-26 11:48:16 +0000

    Volvemos a local

--- conexion.php
+++ conexion.php
@@ -1,11 +1,11 @@
 <?php
-$username = "remoto";
-$password = "@lexand3rCS1998_rem0t0_mysql";
+$username = "root";
+$password = "";
 
 try{
 
     // Create connection
-    $conex = new PDO('mysql:host=161.22.47.138; dbname=fascanfood', $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "set names UTF8"));
+    $conex = new PDO('mysql:host=localhost; dbname=fascanfood', $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "set names UTF8"));
 
 } catch (Exception $e) {
     
